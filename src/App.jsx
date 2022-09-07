0fb830c6062272e0235009fff37306b733fa55d0
Paulo Eduardo Dias da Silva <pauloeddias@gmail.com>
2022-09-07 01:50:54 +0000

    Adding functionality to interact with the deployed contract

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import abi from "./utils/WavePortal.json";
+import { ethers } from "ethers";
+
+const contractAddress = "0x8dF4596D1dE70179d10B2f3Dd5Ef1D7B2ae0DcBc";
+const contractABI = abi.abi;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
@@ -50,6 +55,37 @@ const App = () => {
     }
   }
 
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Mining...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, [])
@@ -65,7 +101,7 @@ const App = () => {
           I am Paulo and I work with machine learning. Cool, right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={null}>
+        <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
 
